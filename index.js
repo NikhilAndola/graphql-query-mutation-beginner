b0461e4fdafb95de8b8e854ba9c55f42f5653ea7
nikhil andola <nikhilandola123@gmail.com>
2022-06-21 02:39:33 +0530

    modified folder structure

--- index.js
+++ index.js
@@ -1,67 +1,68 @@
 const express = require("express");
 const app = express();
 const PORT = 6969;
-const userData = require("./MOCK_DATA.json");
-const graphql = require("graphql");
-const {
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLInt,
-  GraphQLString,
-  GraphQLList,
-} = graphql;
+// const userData = require("./MOCK_DATA.json");
+// const graphql = require("graphql");
+// const {
+//   GraphQLObjectType,
+//   GraphQLSchema,
+//   GraphQLInt,
+//   GraphQLString,
+//   GraphQLList,
+// } = graphql;
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./Schemas")
 
-const UserType = new GraphQLObjectType({
-  name: "User",
-  fields: () => ({
-    id: { type: GraphQLInt },
-    firstName: { type: GraphQLString },
-    lastName: { type: GraphQLString },
-    email: { type: GraphQLString },
-    password: { type: GraphQLString },
-  }),
-});
+// const UserType = new GraphQLObjectType({
+//   name: "User",
+//   fields: () => ({
+//     id: { type: GraphQLInt },
+//     firstName: { type: GraphQLString },
+//     lastName: { type: GraphQLString },
+//     email: { type: GraphQLString },
+//     password: { type: GraphQLString },
+//   }),
+// });
 
-const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  fields: {
-    getAllUsers: {
-      type: new GraphQLList(UserType),
-      args: { id: { type: GraphQLInt } },
-      resolve(parent, args) {
-        return userData;
-      },
-    },
-  },
-});
+// const RootQuery = new GraphQLObjectType({
+//   name: "RootQueryType",
+//   fields: {
+//     getAllUsers: {
+//       type: new GraphQLList(UserType),
+//       args: { id: { type: GraphQLInt } },
+//       resolve(parent, args) {
+//         return userData;
+//       },
+//     },
+//   },
+// });
 
-const Mutation = new GraphQLObjectType({
-  name: "Mutation",
-  fields: {
-    createUser: {
-      type: UserType,
-      args: {
-        firstName: { type: GraphQLString },
-        lastName: { type: GraphQLString },
-        email: { type: GraphQLString },
-        password: { type: GraphQLString },
-      },
-      resolve(parent, args) {
-        userData.push({
-          id: userData.length + 1,
-          firstName: args.firstName,
-          lastName: args.lastName,
-          email: args.email,
-          password: args.password,
-        });
-        return args;
-      },
-    },
-  },
-});
+// const Mutation = new GraphQLObjectType({
+//   name: "Mutation",
+//   fields: {
+//     createUser: {
+//       type: UserType,
+//       args: {
+//         firstName: { type: GraphQLString },
+//         lastName: { type: GraphQLString },
+//         email: { type: GraphQLString },
+//         password: { type: GraphQLString },
+//       },
+//       resolve(parent, args) {
+//         userData.push({
+//           id: userData.length + 1,
+//           firstName: args.firstName,
+//           lastName: args.lastName,
+//           email: args.email,
+//           password: args.password,
+//         });
+//         return args;
+//       },
+//     },
+//   },
+// });
 
-const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
+// const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
 app.use(
   "/graphql",
